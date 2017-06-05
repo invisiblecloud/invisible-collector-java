bc2f2d94c53c9a9ebf329651424dd232d7fb3e81
João Madureira <jpmadureira@gmail.com>
2017-06-05 10:37:04 +0100

    Added a Company model.

---
+++ src/main/java/com/ic/invoicecapture/model/Company.java
@@ -0,0 +1,100 @@
+package com.ic.invoicecapture.model;
+
+import java.util.Objects;
+
+public class Company {
+
+    private String name;
+    private String gid;
+    private String vatNumber;
+    private String address;
+    private String zipCode;
+    private String city;
+    private String country;
+    private Boolean notificationsEnabled;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Company)) {
+            return false;
+        }
+
+        if (this == obj) {
+            return true;
+        }
+
+        Company other = (Company) obj;
+        return Objects.equals(this.vatNumber, other.vatNumber)
+                && Objects.equals(this.name, other.name)
+                && Objects.equals(this.address, other.address)
+                && Objects.equals(this.zipCode, other.zipCode)
+                && Objects.equals(this.city, other.city)
+                && Objects.equals(this.country, other.country)
+                && Objects.equals(this.gid, other.gid)
+                && Objects.equals(this.notificationsEnabled, other.notificationsEnabled);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getGid() {
+        return gid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVatNumber() {
+        return vatNumber;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public Boolean isNotificationsEnabled() {
+        return notificationsEnabled;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setGid(String gid) {
+        this.gid = gid;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNotificationsEnabled(Boolean notificationsEnabled) {
+        this.notificationsEnabled = notificationsEnabled;
+    }
+
+    public void setVatNumber(String vatNumber) {
+        this.vatNumber = vatNumber;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
---
+++ src/test/java/com/is/invoicecapture/model/CompanyTest.java
@@ -0,0 +1,20 @@
+package com.is.invoicecapture.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+import com.ic.invoicecapture.model.Company;
+
+public class CompanyTest {
+
+    @Test
+    public void testEquals() {
+        Company c1 = new Company();
+        Company c2 = new Company();
+        assertEquals(c1, c2);
+        c1.setName("some other name");
+        assertNotEquals(c1, c2);
+    }
+}
