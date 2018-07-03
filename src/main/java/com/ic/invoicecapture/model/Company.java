d0b8f0a94d5875a843cee44ba046e41fedf0d952
Rostyslav Khoptiy <ros65536@gmail.com>
2018-07-03 17:40:44 +0100

    add style checker, refactor source to google java style guide
    - add checkstyle to maven pom.xml build phase
    - apply google style guide to checkstyle

--- src/main/java/com/ic/invoicecapture/model/Company.java
+++ src/main/java/com/ic/invoicecapture/model/Company.java
@@ -4,103 +4,102 @@ import java.util.Objects;
 
 public class Company {
 
-    private String name;
-    private String gid;
-    private String vatNumber;
-    private String address;
-    private String zipCode;
-    private String city;
-    private String country;
-    private Boolean notificationsEnabled;
+  private String name;
+  private String gid;
+  private String vatNumber;
+  private String address;
+  private String zipCode;
+  private String city;
+  private String country;
+  private Boolean notificationsEnabled;
 
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof Company)) {
-            return false;
-        }
-
-        if (this == obj) {
-            return true;
-        }
-
-        Company other = (Company) obj;
-        return Objects.equals(this.vatNumber, other.vatNumber)
-                && Objects.equals(this.name, other.name)
-                && Objects.equals(this.address, other.address)
-                && Objects.equals(this.zipCode, other.zipCode)
-                && Objects.equals(this.city, other.city)
-                && Objects.equals(this.country, other.country)
-                && Objects.equals(this.gid, other.gid)
-                && Objects.equals(this.notificationsEnabled, other.notificationsEnabled);
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Company)) {
+      return false;
     }
 
-    @Override
-    public int hashCode() {
-    	return Objects.hash(this.vatNumber, this.name, this.address, this.zipCode, this.city, this.country, this.gid, this.notificationsEnabled);
-    }
-    
-    
-    public String getAddress() {
-        return address;
+    if (this == obj) {
+      return true;
     }
 
-    public String getCity() {
-        return city;
-    }
+    Company other = (Company) obj;
+    return Objects.equals(this.vatNumber, other.vatNumber)
+        && Objects.equals(this.name, other.name)
+        && Objects.equals(this.address, other.address)
+        && Objects.equals(this.zipCode, other.zipCode) && Objects.equals(this.city, other.city)
+        && Objects.equals(this.country, other.country) && Objects.equals(this.gid, other.gid)
+        && Objects.equals(this.notificationsEnabled, other.notificationsEnabled);
+  }
 
-    public String getCountry() {
-        return country;
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.vatNumber, this.name, this.address, this.zipCode, this.city,
+        this.country, this.gid, this.notificationsEnabled);
+  }
 
-    public String getGid() {
-        return gid;
-    }
 
-    public String getName() {
-        return name;
-    }
+  public String getAddress() {
+    return address;
+  }
 
-    public String getVatNumber() {
-        return vatNumber;
-    }
+  public String getCity() {
+    return city;
+  }
 
-    public String getZipCode() {
-        return zipCode;
-    }
+  public String getCountry() {
+    return country;
+  }
 
-    public Boolean isNotificationsEnabled() {
-        return notificationsEnabled;
-    }
+  public String getGid() {
+    return gid;
+  }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+  public String getVatNumber() {
+    return vatNumber;
+  }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
+  public String getZipCode() {
+    return zipCode;
+  }
 
-    public void setGid(String gid) {
-        this.gid = gid;
-    }
+  public Boolean isNotificationsEnabled() {
+    return notificationsEnabled;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setAddress(String address) {
+    this.address = address;
+  }
 
-    public void setNotificationsEnabled(Boolean notificationsEnabled) {
-        this.notificationsEnabled = notificationsEnabled;
-    }
+  public void setCity(String city) {
+    this.city = city;
+  }
 
-    public void setVatNumber(String vatNumber) {
-        this.vatNumber = vatNumber;
-    }
+  public void setCountry(String country) {
+    this.country = country;
+  }
 
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
+  public void setGid(String gid) {
+    this.gid = gid;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setNotificationsEnabled(Boolean notificationsEnabled) {
+    this.notificationsEnabled = notificationsEnabled;
+  }
+
+  public void setVatNumber(String vatNumber) {
+    this.vatNumber = vatNumber;
+  }
+
+  public void setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+  }
 }
