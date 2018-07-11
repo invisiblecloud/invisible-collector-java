a5922ba9589eb948cdf69ef0233db085fb775561
Rostyslav Khoptiy <ros65536@gmail.com>
2018-07-11 10:17:35 +0100

    add integrations tests maven build chain

---
+++ src/test/java/com/ic/invoicecapture/model/CompanyIT.java
@@ -0,0 +1,12 @@
+package com.ic.invoicecapture.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CompanyIT {
+  
+  @Test
+  public void correctDeserialization() {
+    Assertions.assertTrue(true);
+  }
+}
