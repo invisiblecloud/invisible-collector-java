c7f3441bdfd7571cfa3794b7d80bba72828960ff
ros <ros65536@gmail.com>
2019-01-08 18:30:35 +0000

    add FindDebtsBuilder date unit tests

--- src/main/java/com/invisiblecollector/model/FindDebtsBuilder.java
+++ src/main/java/com/invisiblecollector/model/FindDebtsBuilder.java
@@ -4,26 +4,30 @@ import java.util.Date;
 
 public class FindDebtsBuilder extends Model {
 
+  private static final String DATE_ERROR_MSG = "to_date must follow from_date";
   public FindDebtsBuilder withFromDate(Date fromDate) {
-    assertDateOrder(fromDate, getToDate(), "to_date must follow from_date");
+
+    assertDateOrder(fromDate, getToDate(), DATE_ERROR_MSG);
     setDate("from_date", fromDate);
     return this;
   }
 
   public FindDebtsBuilder withToDate(Date toDate) {
-    assertDateOrder(getFromDate(), toDate, "to_date must follow from_date");
+    assertDateOrder(getFromDate(), toDate, DATE_ERROR_MSG);
     setDate("to_date", toDate);
     return this;
   }
 
+  private static final String DUE_DATE_ERROR_MSG = "to_duedate must follow from_duedate";
   public FindDebtsBuilder withFromDueDate(Date fromDueDate) {
-    assertDateOrder(fromDueDate, getToDueDate(), "to_duedate must follow from_duedate");
+
+    assertDateOrder(fromDueDate, getToDueDate(), DUE_DATE_ERROR_MSG);
     setDate("from_duedate", fromDueDate);
     return this;
   }
 
   public FindDebtsBuilder withToDueDate(Date toDueDate) {
-    assertDateOrder(getFromDueDate(), toDueDate, "to_duedate must follow from_duedate");
+    assertDateOrder(getFromDueDate(), toDueDate, DUE_DATE_ERROR_MSG);
     setDate("to_duedate", toDueDate);
     return this;
   }
